8292a88992f124007f40e63537433f06c697ac55
Tj <devultj@gmail.com>
2017-07-20 20:40:57 +0100

    Revert "Action bar animations"
    This reverts commit 20f658f394717b9bd8e41e55d23ca9500c50e53c.

--- lua/dheists/cl_actions.lua
+++ lua/dheists/cl_actions.lua
@@ -26,58 +26,53 @@ dHeists.ActionColor = Color( 255, 0, 0 )
 dHeists.ActionStart = 0
 dHeists.ActionEnd = 0
 
-local isDying = false
-
-local scrW, scrH = ScrW(), ScrH()
-local w, h = scrW * 0.35, 28
-local xPos = - w
-
 function dHeists.DrawAction()
 	local actionColor = dHeists.ActionColor
 	local start, finish = dHeists.ActionStart, dHeists.ActionEnd
 	local curTime = CurTime()
+	local scrW, scrH = ScrW(), ScrH()
 
-	local timeRemainingText = dHeists.ActionTimeRemainingText or "TIME REMAINING"  
-	local fraction = 1 - math.TimeFraction( start, finish, curTime )
-	if fraction <= 0 then isDying = true end
+	local timeRemainingText = dHeists.ActionTimeRemainingText or "TIME REMAINING"   
 
-	if dHeists.ActionDelete or isDying then
-		xPos = Lerp(10 * FrameTime(), xPos, - w )
+	if finish > curTime then
+		local fraction = 1 - math.TimeFraction( start, finish, curTime )
+		local alpha = fraction * 255
 
-		if xPos < - ( w - 2 ) then
-			isDying = false
-			xPos = - w
-			dHeists.ActionDelete = nil
+		if alpha > 0 then
+			local w, h = scrW * 0.35, 28
+			local x, y = ( scrW * 0.5 ) - ( w * 0.5 ), ( scrH * 0.725 ) - ( h * 0.5 )
+
+			surface.SetDrawColor( 35, 35, 35, 100 )
+			surface.DrawRect( x, y, w, h )
+
+			surface.SetDrawColor( 0, 0, 0, 120 )
+			surface.DrawOutlinedRect( x, y, w, h )
+
+			surface.SetDrawColor( Color( 0, 0, 0, 100 ) )
+			surface.DrawRect(x + 4, y + 4, ( w * fraction ) - 8, h - 8 )
+
+			surface.SetDrawColor( actionColor.r, actionColor.g, actionColor.b, 200 )
+			surface.DrawRect( x + 4, y + 4, ( w * fraction ) - 8, h - 8 )
+
+			draw.SimpleText( timeRemainingText, "dHeistsMedium", x + 2, y - 22, color_black )
+			draw.SimpleText( timeRemainingText, "dHeistsMedium", x, y - 24, color_white )
+
+			local remainingTime = string.FormattedTime( math.max( finish - curTime, 0 ), "%02i:%02i:%02i" )
+			draw.SimpleText( remainingTime, "dHeistsMedium", x + w, y - 22, color_black, TEXT_ALIGN_RIGHT )
+			draw.SimpleText( remainingTime, "dHeistsMedium", x + w, y - 24, color_white, TEXT_ALIGN_RIGHT )
 		end
-	else
-		xPos = Lerp( 10 * FrameTime(), xPos, ( ( scrW * 0.5 ) - ( w * 0.5 ) ) )
 	end
-
-	local x = xPos
-	local y = ( scrH * 0.725 ) - ( h * 0.5 )
-
-	surface.SetDrawColor( 35, 35, 35, 100 )
-	surface.DrawRect( x, y, w, h )
-
-	surface.SetDrawColor( 0, 0, 0, 120 )
-	surface.DrawOutlinedRect( x, y, w, h )
-
-	surface.SetDrawColor( Color( 0, 0, 0, 100 ) )
-	surface.DrawRect(x + 4, y + 4, ( w * fraction ) - 8, h - 8 )
-
-	surface.SetDrawColor( actionColor.r, actionColor.g, actionColor.b, 200 )
-	surface.DrawRect( x + 4, y + 4, ( w * fraction ) - 8, h - 8 )
-
-	draw.SimpleText( timeRemainingText, "dHeistsMedium", x + 2, y - 22, color_black )
-	draw.SimpleText( timeRemainingText, "dHeistsMedium", x, y - 24, color_white )
-
-	local remainingTime = string.FormattedTime( math.max( finish - curTime, 0 ), "%02i:%02i:%02i" )
-	draw.SimpleText( remainingTime, "dHeistsMedium", x + w, y - 22, color_black, TEXT_ALIGN_RIGHT )
-	draw.SimpleText( remainingTime, "dHeistsMedium", x + w, y - 24, color_white, TEXT_ALIGN_RIGHT )
 end
 
 function dHeists.ClearAction()
-	dHeists.ActionDelete = true
+	dHeists.StateText = ""
+	dHeists.ActionText = ""
+	dHeists.ActionColor = Color( 255, 0, 0 )
+	dHeists.ActionStart = 0
+	dHeists.ActionEnd = 0
+	dHeists.ActionIdentifier = nil
+	dHeists.HideBigText = nil
+	dHeists.ActionTimeRemainingText = nil
 end
 
 hook.Add( "HUDPaint", "dHeists.Action", function()
@@ -121,8 +116,6 @@ end)
 hook.Add( "HUDPaint", "dHeists.Actions", function()
 	local player = LocalPlayer()
 	if not player._dHeistsActionData then return end
-	
-	if dHeists.ActionDelete then return end
 
 	local extraData = player._dHeistsActionData
 	if IsValid( extraData.ent ) then
