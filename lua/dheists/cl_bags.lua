d55ee7bce6745642ecd24e1b490220b701556d5e
Tj <devultj@gmail.com>
2017-07-20 19:31:23 +0100

    Added bag UI

--- lua/dheists/cl_bags.lua
+++ lua/dheists/cl_bags.lua
@@ -20,10 +20,41 @@ hook.Add( "CalcView", "dHeists.drawBag", function( player, origin, angles, fov )
 
         local view = {
             origin = origin,
-            angles = Angle( angles.p, angles.y, angles.r + roll ),
-            fov = fov - 5
+            angles = Angle( angles.p, angles.y, angles.r + roll )
         }
 
         return view
+    end
+end )
+
+surface.CreateFont( "dHeists_bagText", {
+    font = "Roboto Condensed",
+    weight = 800,
+    size = 26,
+} )
+
+local hudX, hudY = ScrW() - 4, ScrH() * 0.6
+local width, height = 230, 70
+
+local throwText =  "[" .. "G" .. "] TO THROW"
+local carryingText = "CARRYING:"
+
+local posMult = 0
+hook.Add( "HUDPaint", "dHeists.drawBag", function()
+    if posMult ~= 0 or ( posMult == 0 and LocalPlayer():GetNW2Bool( "dHeists_CarryingBag", false ) ) then
+        posMult = Lerp( FrameTime() * 20, posMult, LocalPlayer():GetNW2Bool( "dHeists_CarryingBag", false ) and 1 or 0 )
+
+        local offset = ( width * 1 ) / posMult
+
+        surface.DrawCuteRect( hudX - offset, hudY, width, height, 3, 100 )
+
+        draw.SimpleText( throwText, "dHeists_bagText", hudX + width + 2 - offset, hudY - 26, Color( 0, 0, 0, 185 ), TEXT_ALIGN_RIGHT )
+        draw.SimpleText( throwText, "dHeists_bagText", hudX + width - offset, hudY - 28, Color( 240, 240, 240, 255 ), TEXT_ALIGN_RIGHT )
+
+        draw.SimpleText( carryingText, "dHeists_bagText", hudX + 10 - offset, hudY + 8, Color( 0, 0, 0, 185 ) )
+        draw.SimpleText( carryingText, "dHeists_bagText", hudX + 8 - offset, hudY + 6, color_white )
+
+        draw.SimpleText( "BAG OF CASH", "dHeists_bagText", hudX + 8 - offset, hudY + 36, Color( 0, 0, 0, 185 ) )
+        draw.SimpleText( "BAG OF CASH", "dHeists_bagText", hudX + 6 - offset, hudY + 34, color_white )
     end
 end )
---
+++ lua/dheists/cl_ui.lua
@@ -0,0 +1,26 @@
+--[[
+    © 2017 devultj.co, do not share, re-distribute or modify
+
+    without permission of its author (devultj@gmail.com).
+]]
+
+function surface.DrawCuteRect(x, y, w, h, gasp, alpha)
+    if (not gasp) then
+        gasp = 4
+    end
+    if (! alpha) then
+        alpha = 100
+    end
+
+    surface.SetDrawColor(0, 0, 0, alpha)
+    surface.DrawRect(x, y, w, h)
+    surface.SetDrawColor(255, 255, 255, 255)
+    surface.DrawRect(x, y, 4 * gasp, gasp)
+    surface.DrawRect(x + w - 4 * gasp, y, 4 * gasp, gasp)
+    surface.DrawRect(x, y + h - gasp, 4 * gasp, gasp)
+    surface.DrawRect(x + w - 4 * gasp, y + h - gasp, 4 * gasp, gasp)
+    surface.DrawRect(x, y + gasp, gasp, 4 * gasp)
+    surface.DrawRect(x + w - gasp, y + gasp, gasp, 4 * gasp - gasp)
+    surface.DrawRect(x, y + gasp + h - 4 * gasp - gasp, gasp, 4 * gasp - gasp)
+    surface.DrawRect(x + w - gasp, y + gasp + h - 4 * gasp - gasp, gasp, 4 * gasp - gasp)
+end
