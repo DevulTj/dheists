41d5e5fc4e70dd9966eb0b01c1949abc7183a26a
Tj <devultj@gmail.com>
2017-07-23 14:11:13 +0100

    Added loot HUD

--- lua/dheists/entities/sh_loot_base.lua
+++ lua/dheists/entities/sh_loot_base.lua
@@ -89,6 +89,22 @@ if CLIENT then
     	self:DrawModel()
     end
 
+	local drawTextDistance = 160000
+	hook.Add( "HUDPaint", "dHeists.loot", function()
+		local entity = LocalPlayer():GetEyeTrace().Entity
+		if not IsValid( entity ) or not entity.IsLoot or entity:GetPos():DistToSqr( LocalPlayer():GetPos() ) > drawTextDistance then return end
+
+		local lootData = dHeists.loot.list[ entity:GetLootType() ]
+		if not lootData then return end
+
+		local pos = entity:GetPos()
+		pos.z = pos.z + 10
+		pos = pos:ToScreen()
+
+		draw.SimpleText( lootData.name, "dHeists_bagText", pos.x + 1, pos.y + 1, color_black, TEXT_ALIGN_CENTER )
+		draw.SimpleText( lootData.name, "dHeists_bagText", pos.x, pos.y, color_white, TEXT_ALIGN_CENTER )
+	end )
+
 	function ENT:Initialize()
         self:DrawShadow( false )
 	end
