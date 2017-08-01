cd4a34c757169b6f9d5c72566401d2fa4698b756
Tj <devultj@gmail.com>
2017-08-01 15:40:27 +0100

    Added new fonts

--- lua/dheists/module/cl_fonts.lua
+++ lua/dheists/module/cl_fonts.lua
@@ -4,6 +4,13 @@
 	without permission of its author (devultj@gmail.com).
 ]]
 
+surface.CreateFont( "dHeistsLarge", {
+	font = dHeists.config.fontFace or "Purista",
+	size = 32,
+	weight = dHeists.config.fontWeight or 800,
+	antialias = true
+} )
+
 surface.CreateFont( "dHeistsMedium", {
 	font = dHeists.config.fontFace or "Purista",
 	size = 24,
@@ -15,6 +22,14 @@ surface.CreateFont( "dHeists_bagText", {
     font = dHeists.config.fontFace or "Purista",
     weight = 800,
     size = 26,
+} )
+
+surface.CreateFont( "dHeists_bagTextItalics", {
+    font = dHeists.config.fontFace or "Purista",
+	weight = 800,
+	size = 20,
+	italic = true,
+	strikeout = true,
 } )
 
 surface.CreateFont( "dHeists_bagText3D", {
