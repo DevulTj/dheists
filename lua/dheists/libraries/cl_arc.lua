762a5daffeff5aa50287663ed616fa2b4c99ae28
Tj <devultj@gmail.com>
2017-08-01 15:40:11 +0100

    New arc functions

--- lua/dheists/libraries/cl_arc.lua
+++ lua/dheists/libraries/cl_arc.lua
@@ -19,10 +19,10 @@ end
 
 function surface.PrecacheArc(cx,cy,radius,thickness,startang,endang,roughness)
 	local quadarc = {}
-	
+
 	-- Correct start/end ang
 	local startang,endang = startang or 0, endang or 0
-	
+
 	-- Define step
 	-- roughness = roughness or 1
 	local diff = abs(startang-endang)
@@ -31,18 +31,18 @@ function surface.PrecacheArc(cx,cy,radius,thickness,startang,endang,roughness)
 	if startang > endang then
 		step = abs(step) * -1
 	end
-	
+
 	-- Create the inner circle's points.
 	local inner = {}
 	local outer = {}
 	local ct = 1
 	local r = radius - thickness
-	
+
 	for deg=startang, endang, step do
 		local rad = rad1(deg)
 		-- local rad = deg2rad * deg
 		local cosrad, sinrad = cos(rad), sin(rad) --calculate sin,cos
-		
+
 		local ox, oy = cx+(cosrad*r), cy+(-sinrad*r) --apply to inner distance
 		inner[ct] = {
 			x=ox,
@@ -50,7 +50,7 @@ function surface.PrecacheArc(cx,cy,radius,thickness,startang,endang,roughness)
 			u=(ox-cx)/radius + .5,
 			v=(oy-cy)/radius + .5,
 		}
-		
+
 		local ox2, oy2 = cx+(cosrad*radius), cy+(-sinrad*radius) --apply to outer distance
 		outer[ct] = {
 			x=ox2,
@@ -58,10 +58,10 @@ function surface.PrecacheArc(cx,cy,radius,thickness,startang,endang,roughness)
 			u=(ox2-cx)/radius + .5,
 			v=(oy2-cy)/radius + .5,
 		}
-		
+
 		ct = ct + 1
 	end
-	
+
 	-- QUAD the points.
 	for tri=1,ct do
 		local p1,p2,p3,p4
@@ -70,11 +70,80 @@ function surface.PrecacheArc(cx,cy,radius,thickness,startang,endang,roughness)
 		p2=outer[t]
 		p3=inner[t]
 		p4=inner[tri]
-		
+
 		quadarc[tri] = {p1,p2,p3,p4}
 	end
-	
+
 	-- Return a table of triangles to draw.
 	return quadarc
-	
+
+end
+
+function draw.DrawPercRect(x, y, w, h, perc, sten)
+	local d
+	local rx, ry
+	local px, py
+
+	if (sten) then
+		w, h = w*2, h*2
+		d = w/2
+	else
+		d = math.sqrt(w^2 + h^2)/2
+	end
+
+	px, py = math.sin(perc*math.pi*2), -math.cos(perc*math.pi*2)
+	rx, ry = math.Clamp(x + px*d, x - w/2, x + w/2), math.Clamp(y + py*d, y - h/2, y + h/2)
+
+	if (perc < .25) then
+		triangle = {
+			{ x = rx , y = ry },
+			{ x = x + w/2, y = y - h/2 },
+			{ x = x + w/2, y = y + 0 },
+			{ x = x , y = y + 0 },
+		}
+		surface.DrawPoly( triangle )
+	end
+
+	if (perc < .5) then
+		triangle = {
+			{ x = x , y = y - 0 },
+			{ x = x + w/2, y = y - 0 },
+			{ x = x + w/2, y = y + h/2 },
+			{ x = x , y = y + h/2 },
+		}
+		if (perc > .25) then
+			triangle[2] = { x = rx , y = ry }
+			if (perc > (.25 + .25/2)) then
+				triangle[3] = { x = rx , y = ry }
+			end
+		end
+		surface.DrawPoly( triangle)
+	end
+
+	if (perc < .75) then
+		triangle = {
+			{ x = x - w/2, y = y - 0 },
+			{ x = x + 0, y = y - 0 },
+			{ x = x + 0, y = y + h/2 },
+			{ x = x - w/2, y = y + h/2 },
+		}
+		if (perc > .5) then
+			triangle[3] = { x = rx , y = ry }
+		end
+		surface.DrawPoly( triangle )
+	end
+
+	triangle = {
+		{ x = x - w/2, y = y - h/2 },
+		{ x = x + 0, y = y - h/2 },
+		{ x = x + 0, y = y + 0 },
+		{ x = x - w/2, y = y + 0 },
+	}
+	if (perc > .75) then
+		triangle[4] = { x = rx , y = ry }
+		if (perc > (.75 + .25/2)) then
+			triangle[1] = { x = rx , y = ry }
+		end
+	end
+	surface.DrawPoly( triangle )
 end
