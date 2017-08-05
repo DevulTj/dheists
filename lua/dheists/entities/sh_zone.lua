7617e04934c1edbdd556fc57d2454461ed99827b
Kevin <fruitwasp@users.noreply.github.com>
2017-08-05 14:15:23 +0200

    moved zones non-entity code

--- lua/dheists/entities/sh_zone.lua
+++ lua/dheists/entities/sh_zone.lua
@@ -68,59 +68,6 @@ if SERVER then
             self:SetCooldownTime( math.floor( CurTime() + dHeists.config.get( "zoneCooldownTime" ) ) )
         end
     end
-
-    gameevent.Listen( "player_disconnect" )
-    hook.Add( "player_disconnect", dHeists.IDENTIFIER, function( data )
-        local player = Player( data.userid )
-
-        for zone, _ in pairs( _zones ) do
-            zone:exitPlayer( player )
-        end
-    end )
-
-    hook.Add( "EntityCreated", dHeists.IDENTIFIER .. "zones", function( entity )
-        if entity.IsZone then
-            _zones[ entity ] = true
-        end
-    end )
-
-    hook.Add( "EntityRemoved", dHeists.IDENTIFIER .. "zones", function( entity )
-        if entity.IsZone then
-            _zones[ entity ] = nil
-        end
-    end )
-
-    hook.Add( "PlayerUse", dHeists.IDENTIFIER .. "zones", function( player, entity )
-        if not entity.IsRaidable or not player.inZones then
-            return
-        end
-
-        for zone, _ in pairs( player.inZones ) do
-            if zone:hasCooldown() then
-                return false
-            end
-        end
-    end )
-
-    local PLAYER = FindMetaTable( "Player" )
-
-    function PLAYER:addInZone( zone )
-        if self.inZones[ zone ] then
-
-            return
-        end
-
-        self.inZones[ zone ] = true
-    end
-
-    function PLAYER:removeInZone( zone )
-        if not self.inZones[ zone ] then
-
-            return
-        end
-
-        self.inZones[ zone ] = nil
-    end
 end
 
 if CLIENT then
