fb8724bfcc067fc4c13c8430b141dfd421a2d4c2
Tj <devultj@gmail.com>
2017-09-16 21:23:14 +0100

    Added MySQL credentials file and MySQLite module

---
+++ lua/dheists/config/sv_mysql.lua
@@ -0,0 +1,16 @@
+--[[
+	© 2017 devultj.co.uk, do not share, re-distribute or modify
+
+	without permission of its author (devultj@gmail.com).
+]]
+
+dHeists.config.databaseCredentials = {
+    EnableMySQL = false,
+    Host = "localhost",
+    Username = "root",
+    Password = "",
+    Database_name = "database",
+    Database_port = 3306,
+    Preferred_module = "tmysql4",
+    MultiStatements = false
+}
