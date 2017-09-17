61b4078e3e37e60dde6a9e41f85b9f15971e8857
Tj <devultj@gmail.com>
2017-09-17 05:44:05 +0100

    Added debug clientside zone rendering

--- lua/dheists/module/modules/zones/cl_zones.lua
+++ lua/dheists/module/modules/zones/cl_zones.lua
@@ -3,3 +3,15 @@
 
     without permission of its author (devultj@gmail.com).
 ]]
+
+hook.Add( "PostDrawOpaqueRenderables", "dHeists.zones", function()
+    if not dHeists.config.debugEnabled then return end
+
+    render.SetColorMaterial()
+
+	for sName, tData in pairs( dHeists.zones.list ) do
+        local color = Color( 200, 50, 50, 255 )
+
+        render.DrawWireframeBox( tData.origin, Angle( 0, 0, 0 ), tData.mins, tData.maxs, color, true )
+    end
+end )
