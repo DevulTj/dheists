f9128641a7a4678dc1394cfed44a14718e3028a3
Tj <devultj@gmail.com>
2017-09-17 15:03:10 +0100

    Changed variable consistency in commands

--- lua/dheists/module/modules/commands/sv_commands.lua
+++ lua/dheists/module/modules/commands/sv_commands.lua
@@ -9,47 +9,47 @@ dHeists.commands.list = {}
 
 dHeists.commands.defaultCommandPrefix = "/"
 
-function dHeists.commands:add( tData )
-    local sPrefix = tData.prefix or dHeists.commands.defaultCommandPrefix
-    local sName = tData.name
+function dHeists.commands:add( commandData )
+    local prefix = commandData.prefix or dHeists.commands.defaultCommandPrefix
+    local name = commandData.name
 
-    tData = {
-        command = sName,
-        prefix = sPrefix,
-        canDo = tData.canDo,
-        func = tData.func
+    commandData = {
+        command = name,
+        prefix = prefix,
+        canDo = commandData.canDo,
+        func = commandData.func
     }
 
-    dHeists.commands.list[ sPrefix ] = dHeists.commands.list[ sPrefix ] or {}
-    dHeists.commands.list[ sPrefix ][ sName ] = tData
+    dHeists.commands.list[ prefix ] = dHeists.commands.list[ prefix ] or {}
+    dHeists.commands.list[ prefix ][ name ] = commandData
 end
 
 if SERVER then
-    hook.Add( "PlayerSay", "dHeists.commands.runCommands", function( pPlayer, sText )
-        local sPrefix = sText:sub( 1, 1 ) -- Store prefix
-        sText = sText:sub( 2 ) -- Ditch it in the whole text
+    hook.Add( "PlayerSay", "dHeists.commands.runCommands", function( player, text )
+        local prefix = text:sub( 1, 1 ) -- Store prefix
+        text = text:sub( 2 ) -- Ditch it in the whole text
 
-        sText = string.Explode( " ", sText )
-        if #sText < 1 then return end -- Empty command
+        text = string.Explode( " ", text )
+        if #text < 1 then return end -- Empty command
 
-        if dHeists.commands.list[ sPrefix ] then
-            local sCommand = sText[ 1 ]
-            local tData = dHeists.commands.list[ sPrefix ][ sCommand ] -- Check if the command exists
+        if dHeists.commands.list[ prefix ] then
+            local command = text[ 1 ]
+            local commandData = dHeists.commands.list[ prefix ][ command ] -- Check if the command exists
 
-            table.remove( sText, 1 ) -- Remove the command from the args list
+            table.remove( text, 1 ) -- Remove the command from the args list
 
-            if tData then
-                local canDo, reason = tData.canDo and tData.canDo( pPlayer, tData )
+            if commandData then
+                local canDo, reason = commandData.canDo and commandData.canDo( player, commandData )
                 if canDo == false then
                     if reason then
-                        dHeists.addNotification( pPlayer, reason )
+                        dHeists.addNotification( player, reason )
                     end
 
                     return ""
                 end
 
-                tData.func( pPlayer, sText, tData ) -- Execute the command
-                hook.Run( "dHeists.commands.onCommand", pPlayer, sText, tData ) -- Hook support
+                commandData.func( player, text, commandData ) -- Execute the command
+                hook.Run( "dHeists.commands.onCommand", player, text, commandData ) -- Hook support
 
                 return ""
             end
