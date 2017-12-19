7fa9c0c4287e7b3709f077f9060a8b52fbcceeb1
Tj <devultj@gmail.com>
2017-12-19 02:36:17 +0000

    Altered phrasing on mask drops

--- lua/dheists/module/modules/masks/cl_masks.lua
+++ lua/dheists/module/modules/masks/cl_masks.lua
@@ -11,12 +11,12 @@ hook.Add( "HUDPaint", dHeists.IDENTIFIER .. "_mask", function()
     if #LocalPlayer():getDevString( "currentMask", nil ) ~= 0 then
         local isEquipped = LocalPlayer():getDevBool( "maskEquipped", false )
         local keyName = input.GetKeyName( dHeists.config.maskEquipKey ):upper()
-        local throwText = ("[%s] TO %s MASK"):format( keyName, isEquipped and "UN-EQUIP" or "EQUIP" )
+        local throwText = ("[%s] %s MASK"):format( keyName, isEquipped and "UN-EQUIP" or "EQUIP" )
 
         if not isEquipped then
-            local dropText = ("[SHIFT+%s] TO DROP MASK"):format( keyName )
-            draw.SimpleText( dropText, "dHeists_bagText", hudX + 2, hudY - 26 - 26, Color( 0, 0, 0, 185 ), TEXT_ALIGN_RIGHT )
-            draw.SimpleText( dropText, "dHeists_bagText", hudX, hudY - 26 - 28, Color( 240, 240, 240, 255 ), TEXT_ALIGN_RIGHT )
+            local dropText = ("[SHIFT+%s] DROP"):format( keyName )
+            draw.SimpleText( dropText, "dHeists_bagTextItalics", hudX + 2, hudY - 26 - 18, Color( 0, 0, 0, 185 ), TEXT_ALIGN_RIGHT )
+            draw.SimpleText( dropText, "dHeists_bagTextItalics", hudX, hudY - 26 - 20, Color( 170, 255, 170, 255 ), TEXT_ALIGN_RIGHT )
         end
 
         draw.SimpleText( throwText, "dHeists_bagText", hudX + 2, hudY - 26, Color( 0, 0, 0, 185 ), TEXT_ALIGN_RIGHT )
