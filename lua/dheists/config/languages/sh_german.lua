312637f9a1f6811e309f823518cb7c49cacaaebe
Tj <devultj@gmail.com>
2018-01-26 21:22:42 +0000

    Added WIP german localization

---
+++ lua/dheists/config/languages/sh_german.lua
@@ -0,0 +1,39 @@
+--[[
+	© 2018 devultj.co.uk, do not share, re-distribute or modify
+
+	without permission of its author (devultj@gmail.com).
+]]
+
+i18n.registerPhrases( "de", {
+    -- bag names
+    bag_regular = "Regular Bag",
+    bag_luxury = "Luxury Bag",
+    bag_enhanced = "Enhanced Bag",
+
+    -- loot names
+    loot_cash_roll_small = "Small Roll of Cash",
+    loot_golden_figure = "SecuroServ Golden Figure",
+    loot_silver_figure = "SecuroServ Silver Figure",
+    loot_cash_case = "Case of Cash",
+    loot_cash_pallet = "Pallet of Cash",
+
+    -- general
+    cooldown_3d = "COOLDOWN",
+    loot_worth = "worth %s",
+
+    stealing_bag = "DIEBSTAHLBEUTEL",
+    picking_up_loot = "BEUTE EINSAMMELN",
+    picking_up_bag = "AUFNAHMETASCHE",
+    confiscating_bag = "CONFISCATING BAG",
+    confiscate_bag_text = "Du hast %s gewonnen, weil du eine Tasche konfisziert hast.",
+    bag_sold_text = "Sie haben %s gewonnen, indem Sie %s verkauft haben."
+
+    -- bags
+    no_items = "NO ITEMS",
+    carrying_text = "CARRYING:",
+    throw_item = "THROW",
+
+    -- masks
+    mask_action_text = "[%s] %s MASK",
+    drop_action_text = "[SHIFT+%s] DROP",
+} )
