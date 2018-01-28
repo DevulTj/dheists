36a1fe9f27f43d0a583915b496527c4c560405d8
Flixs <shawn.f3@t-online.de>
2018-01-28 02:09:18 +0100

    Added new content system

--- lua/dheists/module/sv_content.lua
+++ lua/dheists/module/sv_content.lua
@@ -4,6 +4,58 @@
 	without permission of its author (devultj@gmail.com).
 ]]
 
-resource.AddWorkshop( "970777104" ) -- dHeists
-resource.AddWorkshop( "357350543" ) -- PAYDAY bags
-resource.AddWorkshop( "941544793" ) -- Office GTAV
+local ADDON_PREFIX = "[Content-Manager]"
+local function printDebug( sText )
+    dHeists.print( "${prefix} ${text}" % {
+        prefix = ADDON_PREFIX,
+        text = sText,
+    } )
+end
+
+local ADDON_ADD_RESOURCE = "Added addon resources for ID '${id}'."
+local function printAddResource( sID )
+    printDebug( ADDON_ADD_RESOURCE % {
+        id = sID
+    } )
+end
+
+local ADDON_MISSING = "Missing content detected! Couldn't find addon with ID '${id}'."
+local function printMissingContent( sID )
+    printDebug( ADDON_MISSING % {
+        id = sID
+    } )
+end
+
+local ADDONS = {
+    "941544793",
+    "726820415",
+    "357350543",
+    "970777104"
+}
+
+local function addWorkshopResources()
+    for _, sID in pairs( ADDONS ) do
+        resource.AddWorkshop( sID )
+
+        printAddResource( sID )
+    end
+end
+if not game.SinglePlayer() then
+    addWorkshopResources()
+end
+
+local ADDON_ALL_FOUND = "All addons were found!"
+hook.Add( "InitPostEntity", "dHeits.Module.ContentChecker", function()
+    local tAddonCopy = table.Copy( ADDONS )
+    for _, tData in pairs( engine.GetAddons() ) do
+        table.RemoveByValue( tAddonCopy, tData.wsid )
+    end
+
+    if table.Count( tAddonCopy ) == 0 then
+        printDebug( ADDON_ALL_FOUND )
+    else
+        for _, sID in pairs( tAddonCopy ) do
+            printMissingContent( sID )
+        end
+    end
+end )
