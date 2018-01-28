5a0c26cd0e273ecf190571f105a88de3989507db
Flixs <shawn.f3@t-online.de>
2018-01-28 02:21:54 +0100

    Moved String inter bla bla thing to libaries, to load first

--- lua/dheists/libraries/sh_string.lua
+++ lua/dheists/libraries/sh_string.lua
@@ -23,3 +23,5 @@ function string.formatMoney( amount, currency )
 
     return ( negative and "-" or "" ) .. attachCurrency( amount, currency )
 end
+
+getmetatable("").__mod = function(s, tab) return (s:gsub('($%b{})', function(w) return tab[w:sub(3, -2)] or w end)) end
