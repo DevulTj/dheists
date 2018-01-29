f0269340248072ba339dff8fa2f4636d75676f68
Tj <devultj@gmail.com>
2018-01-29 22:35:00 +0000

    CCTV camera cool effect

--- lua/dheists/module/modules/cctv/cl_cctv.lua
+++ lua/dheists/module/modules/cctv/cl_cctv.lua
@@ -14,6 +14,7 @@ net.Receive( "dHeists_ViewCCTV", function( _ )
     frame:Setup( zoneId )
 end )
 
+local segmentWidth = 32
 function dHeists.cctv.drawCameraHUD( panel, name, pos, ang, w, h )
     panel.offset = name and ( panel.offset or 0 ) or ( panel.offset or 0 ) - 0.5
 
@@ -31,9 +32,11 @@ function dHeists.cctv.drawCameraHUD( panel, name, pos, ang, w, h )
     surface.DrawRect( 0, h - segmentHeight, w, segmentHeight )
     surface.SetDrawColor( 20, 20, 20, 255 )
 
+    segmentWidth = Lerp( FrameTime() * 6, segmentWidth or 32, name and 0 or 32 )
+
     for i = 1, ( w / segmentHeight ) * 3 do
-        surface.DrawRect( i * 64 + panel.offset - 62, 0, 32, segmentHeight )
-        surface.DrawRect( i * 64 + panel.offset - 62, h - segmentHeight, 32, segmentHeight )
+        surface.DrawRect( i * 64 + panel.offset - 62, 0, segmentWidth, segmentHeight )
+        surface.DrawRect( i * 64 + panel.offset - 62, h - segmentHeight, segmentWidth, segmentHeight )
     end
 
     if panel.offset < - width * 2 then
