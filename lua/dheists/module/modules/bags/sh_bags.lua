da79ec0050506c3145cc282ad161892a221e4f4f
Tj <devultj@gmail.com>
2018-02-26 23:50:23 +0000

    Re-enabled jump disabler for bags

--- lua/dheists/module/modules/bags/sh_bags.lua
+++ lua/dheists/module/modules/bags/sh_bags.lua
@@ -45,28 +45,25 @@ hook.Run( "dHeists.bags.registerBag" )
 
 local CMoveData = FindMetaTable( "CMoveData" )
 function CMoveData:RemoveKeys( keys )
-	-- Using bitwise operations to clear the key bits.
-	local newbuttons = bit.band( self:GetButtons(), bit.bnot( keys ) )
-	self:SetButtons( newbuttons )
+    -- Using bitwise operations to clear the key bits.
+    local newbuttons = bit.band( self:GetButtons(), bit.bnot( keys ) )
+    self:SetButtons( newbuttons )
 end
 
--- hook.Add( "SetupMove", "dHeists.setupMoveBags", function( player, moveData, commandData )
---     if player:getBag() then
---         if moveData:KeyDown( IN_JUMP ) then
---             moveData:RemoveKeys( IN_JUMP )
---         end
+hook.Add( "SetupMove", "dHeists.setupMoveBags", function( player, moveData, commandData )
+    if player:getBag() then
+        if moveData:KeyDown( IN_JUMP ) then
+            moveData:RemoveKeys( IN_JUMP )
+        end
 
---         local maxSpeed = moveData:GetMaxSpeed()
+        local reduction = dHeists.config.holdingBagMovementModifierPerItem * #player:getBag().lootItems
+        local defaultModifier = math.Clamp( reduction, 0, dHeists.config.holdingBagMovementModifierMax )
+        local modifier = 1 - defaultModifier
 
---         local reduction = dHeists.config.holdingBagMovementModifierPerItem * #player:getBag().lootItems
-
---         local defaultModifier = math.Clamp( reduction, 0, dHeists.config.holdingBagMovementModifierMax )
---         local modifier = 1 - defaultModifier
-
---         moveData:SetMaxSpeed( moveData:GetMaxSpeed() * modifier )
---         moveData:SetMaxClientSpeed( moveData:GetMaxClientSpeed() * modifier )
---     end
--- end )
+        moveData:SetMaxSpeed( moveData:GetMaxSpeed() * modifier )
+        moveData:SetMaxClientSpeed( moveData:GetMaxClientSpeed() * modifier )
+    end
+end )
 
 -- Include configuration for bags
 frile.includeFile( "dheists/config/config_entities/sh_bags.lua" )
