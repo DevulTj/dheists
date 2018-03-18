8154e8c5ddac23ea09247144d4b4502e9f587557
Tj <devultj@gmail.com>
2018-03-18 19:26:01 +0000

    Added script version ID to all files

--- lua/dheists/libraries/renderobjects/sv_renderobjects.lua
+++ lua/dheists/libraries/renderobjects/sv_renderobjects.lua
@@ -1,6 +1,6 @@
 --[[
 	© 2017 devultj.co.uk, do not share, re-distribute or modify
-	without permission of its author (devultj@gmail.com) - {{ user_id }}
+	without permission of its author (devultj@gmail.com) - {{ user_id }} - Script ID: {{ script_version_name }}
 ]]
 
 renderObjects = renderObjects or {}
--- lua/dheists/module/meta/sh_player.lua
+++ lua/dheists/module/meta/sh_player.lua
@@ -1,7 +1,7 @@
 --[[
 	© 2018 devultj.co.uk, do not share, re-distribute or modify
 
-	without permission of its author (devultj@gmail.com) - {{ user_id }}
+	without permission of its author (devultj@gmail.com) - {{ user_id }} - Script ID: {{ script_version_name }}
 ]]
 
 local PLAYER = FindMetaTable( "Player" )
