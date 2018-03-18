7fe38c0bff7b7c9bd8dfcdad494757497d8a3b12
Tj <devultj@gmail.com>
2018-03-18 13:30:51 +0000

    Added SantosRP support

---
+++ lua/dheists/gamemodes/sh_santosrp.lua
@@ -0,0 +1,21 @@
+--[[
+	© 2018 devultj.co.uk, do not share, re-distribute or modify
+
+	without permission of its author (devultj@gmail.com) - {{ user_id }}
+]]
+
+local SANTOS = {}
+
+SANTOS.name = "Santos RP"
+SANTOS.gamemodeCallback = function() return engine.ActiveGamemode() == "santosrp" end
+
+SANTOS.isPolice = function( player ) return team.GetName( player:Team() ) == "Police" end
+SANTOS.addMoney = function( player, amount ) return player:AddMoney( amount ) end
+SANTOS.getJobName = function( player ) return team.GetName( player:Team() ) end
+SANTOS.getJobCategory = function( player ) return dHeists.gamemodes:getJobList()[ player:Team() ].Name end
+SANTOS.notify = function( player, text, notificationType, notificationTime ) return player:AddNote( text ) end
+
+SANTOS.getJobList = function() return GAMEMODE.Jobs:GetJobs() or {} end
+SANTOS.getCategoryIndex = function() return "category" end
+
+dHeists.gamemodes:addGamemode( SANTOS )
