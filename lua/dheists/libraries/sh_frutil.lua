5ff0f4153b9ad3122f549dffd4b53640834e13fd
Kevin Schuurmans <fruitwasp@users.noreply.github.com>
2018-03-19 16:41:11 +0100

    updated frutil

--- lua/dheists/libraries/sh_frutil.lua
+++ lua/dheists/libraries/sh_frutil.lua
@@ -1,9 +1,30 @@
+
+local version = 1
+
+if frutil and frutil.VERSION >= version then return end
+
+frutil = {
+    VERSION = version,
+    AUTHOR = "fruitwasp",
+    AUTHOR_URL = "https://steamcommunity.com/id/fruitwasp"
+}
+
 local VECTOR = FindMetaTable( "Vector" )
 
 function VECTOR:ceil()
     self.x = math.ceil( self.x )
     self.y = math.ceil( self.y )
     self.z = math.ceil( self.z )
+
+    return self
+end
+
+local ANGLE = FindMetaTable( "Angle" )
+
+function ANGLE:round( decimals )
+    self.pitch = math.Round( self.pitch, decimals )
+    self.yaw = math.Round( self.yaw, decimals )
+    self.roll = math.Round( self.roll, decimals )
 
     return self
 end
